5d15ddf17e89beaf1eb062fa72944cca8ff65aad
ex0dus-0x <ex0dus@codemuch.tech>
2019-07-29 12:18:48 -0400

    Refactor libtrezor build

--- libtrezor/build.sh
+++ libtrezor/build.sh
@@ -4,6 +4,7 @@
 if [ ! -d "trezor-crypto" ]; then
   git clone https://github.com/trezor/trezor-crypto.git
   cd trezor-crypto/
+  cp options.h ed25519-donna/
   make && cd ..
 fi
 
@@ -19,7 +20,7 @@ for test in "${TESTS[@]}"; do
   
   printf "\nBuilding test_$test.cpp with Angora\n"
   deepstate-angora --compile_test test_"$test".cpp --compiler_args="-ltrezor-crypto -lssl -lcrypto" --out_test_name "$WORKSPACE"/"$test" \
-	  --ignore_calls /usr/lib/x86_64-linux-gnu/libssl.a:/home/vagrant/diffcrypt/trezor_crypto/libtrezor-crypto.so:/usr/lib/x86_64-linux-gnu/libcrypto.a
+	  --ignore_calls /usr/lib/x86_64-linux-gnu/libssl.a:$PWD/trezor-crypto/tests/libtrezor-crypto.so:/usr/lib/x86_64-linux-gnu/libcrypto.a
 done
 
 # $(CC) -std=c++11 -g test_* -ltrezor-crypto -lssl -lcrypto
